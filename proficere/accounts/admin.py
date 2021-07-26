b186666543c14d0f15d1f1b9842357c6d6cd2e50
Michael Nadeau <uaedanm@gmail.com>
2021-07-26 13:54:52 -0400

    Made Bootstap5 changes to navbar.html and admin.py

--- proficere/accounts/admin.py
+++ proficere/accounts/admin.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import Group
 # Register your models here.
 from .models import *
 
+@admin.register(SecurityQuestion)
 class SecurityQuestionAdmin(admin.ModelAdmin):
     fields = ['securityquestion', 'active', 'startdate','enddate']
     def save_model(self, request, obj, form, change):
@@ -19,7 +20,8 @@ class SecurityQuestionAdmin(admin.ModelAdmin):
 #    search_fields = ('securityquestion')
     ordering = ('id',)
     list_filter = ('active', 'startdate', 'lastmodifydate',)
-    
+
+@admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
     fields = ['user', 'birthdate', 'userphoto', 'securityquestion01', 'securityanswer01', 'active', 'startdate', 'enddate']
     def save_model(self, request, obj, form, change):
@@ -33,7 +35,8 @@ class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'birthdate', 'userphoto', 'active', 'startdate', 'lastmodifydate')
     ordering = ('user',)
     list_filter = ('active', 'startdate', 'lastmodifydate',)
-    
+
+@admin.register(AddressType)
 class AddressTypeAdmin(admin.ModelAdmin):
     fields = ['addresstype', 'active', 'startdate', 'enddate']
     def save_model(self, request, obj, form, change):
@@ -47,7 +50,8 @@ class AddressTypeAdmin(admin.ModelAdmin):
     list_display = ('addresstype', 'active', 'startdate', 'lastmodifydate')
     ordering = ('id',)
     list_filter = ('active', 'startdate', 'lastmodifydate',)
-    
+
+@admin.register(Country)
 class CountryAdmin(admin.ModelAdmin):
     fields = ['country', 'active', 'startdate', 'enddate']
     def save_model(self, request, obj, form, change):
@@ -61,7 +65,8 @@ class CountryAdmin(admin.ModelAdmin):
     list_display = ('country', 'active', 'startdate', 'lastmodifydate')
     ordering = ('id',)
     list_filter = ('active', 'startdate', 'lastmodifydate',)
-    
+
+@admin.register(State)
 class StateAdmin(admin.ModelAdmin):
     fields = ['country', 'state', 'statename', 'active', 'startdate', 'enddate']
     def save_model(self, request, obj, form, change):
@@ -75,7 +80,8 @@ class StateAdmin(admin.ModelAdmin):
     list_display = ('state', 'statename','country',  'active', 'startdate', 'lastmodifydate')
     ordering = ('country', 'state',)
     list_filter = ('active', 'startdate', 'lastmodifydate',)
-    
+
+@admin.register(Address)
 class AddressAdmin(admin.ModelAdmin):
     fields = ['addresstype', 'country', 'addressline01', 'addressline02', 'addressline03', 'city', 'state', 'postalcode', 'active', 'startdate', 'enddate']
     def save_model(self, request, obj, form, change):
@@ -89,7 +95,8 @@ class AddressAdmin(admin.ModelAdmin):
     list_display = ('addresstype', 'country', 'city', 'state', 'addressline01', 'active', 'startdate', 'lastmodifydate')
     ordering = ('addresstype', 'country', 'city', 'state', 'addressline01',)
     list_filter = ('active', 'startdate', 'lastmodifydate',)
-    
+
+@admin.register(UserAddress)
 class UserAddressAdmin(admin.ModelAdmin):
     fields = ['user', 'address', 'primaryuseraddress', 'active', 'startdate', 'enddate']
     def save_model(self, request, obj, form, change):
@@ -103,7 +110,8 @@ class UserAddressAdmin(admin.ModelAdmin):
     list_display = ('user', 'address', 'primaryuseraddress', 'active', 'startdate', 'lastmodifydate')
     ordering = ('user', 'primaryuseraddress', 'address',)
     list_filter = ('active', 'startdate', 'lastmodifydate',)
-    
+
+@admin.register(PhoneType)
 class PhoneTypeAdmin(admin.ModelAdmin):
     fields = ['phonetype', 'active', 'startdate', 'enddate']
     def save_model(self, request, obj, form, change):
@@ -117,7 +125,8 @@ class PhoneTypeAdmin(admin.ModelAdmin):
     list_display = ('phonetype', 'active', 'startdate', 'lastmodifydate')
     ordering = ('id',)
     list_filter = ('active', 'startdate', 'lastmodifydate',)
-    
+
+@admin.register(CountryExchange)
 class CountryExchangeAdmin(admin.ModelAdmin):
     fields = ['country', 'countryexchange', 'active', 'startdate', 'enddate']
     def save_model(self, request, obj, form, change):
@@ -131,7 +140,8 @@ class CountryExchangeAdmin(admin.ModelAdmin):
     list_display = ('country', 'countryexchange', 'active', 'startdate', 'lastmodifydate')
     ordering = ('countryexchange',)
     list_filter = ('active', 'startdate', 'lastmodifydate',)
-    
+
+@admin.register(Phone)
 class PhoneAdmin(admin.ModelAdmin):
     fields = ['phonetype', 'countryexchange', 'phoneno', 'active', 'startdate', 'enddate']
     def save_model(self, request, obj, form, change):
@@ -145,7 +155,8 @@ class PhoneAdmin(admin.ModelAdmin):
     list_display = ('phonetype', 'countryexchange', 'phoneno', 'active', 'startdate', 'lastmodifydate')
     ordering = ('id',)
     list_filter = ('active', 'startdate', 'lastmodifydate',)
-    
+
+@admin.register(UserPhone)
 class UserPhoneAdmin(admin.ModelAdmin):
     fields = ['user', 'phone', 'primaryuserphone', 'active', 'startdate', 'enddate']
     def save_model(self, request, obj, form, change):
@@ -159,7 +170,8 @@ class UserPhoneAdmin(admin.ModelAdmin):
     list_display = ('user', 'phone', 'primaryuserphone', 'active', 'startdate', 'lastmodifydate')
     ordering = ('user', 'primaryuserphone', 'phone',)
     list_filter = ('active', 'startdate', 'lastmodifydate',)
-    
+
+@admin.register(EmailType)
 class EmailTypeAdmin(admin.ModelAdmin):
     fields = ['emailtype', 'active', 'startdate', 'enddate']
     def save_model(self, request, obj, form, change):
@@ -173,7 +185,8 @@ class EmailTypeAdmin(admin.ModelAdmin):
     list_display = ('emailtype', 'active', 'startdate', 'lastmodifydate')
     ordering = ('id',)
     list_filter = ('active', 'startdate', 'lastmodifydate',)
-    
+
+@admin.register(EmailAddress)
 class EmailAddressAdmin(admin.ModelAdmin):
     fields = ['emailtype', 'emailaddress', 'active', 'startdate', 'enddate']
     def save_model(self, request, obj, form, change):
@@ -189,17 +202,17 @@ class EmailAddressAdmin(admin.ModelAdmin):
     list_filter = ('active', 'startdate', 'lastmodifydate',)
 
 
-admin.site.site_header = 'Proficere Admin Dashboard'
-admin.site.register(SecurityQuestion, SecurityQuestionAdmin)
-admin.site.register(UserProfile, UserProfileAdmin)
-admin.site.register(AddressType, AddressTypeAdmin)
-admin.site.register(Country, CountryAdmin)
-admin.site.register(State, StateAdmin)
-admin.site.register(Address, AddressAdmin)
-admin.site.register(UserAddress, UserAddressAdmin)
-admin.site.register(PhoneType, PhoneTypeAdmin)
-admin.site.register(CountryExchange, CountryExchangeAdmin)
-admin.site.register(Phone, PhoneAdmin)
-admin.site.register(UserPhone, UserPhoneAdmin)
-admin.site.register(EmailType, EmailTypeAdmin)
-admin.site.register(EmailAddress, EmailAddressAdmin)
+#admin.site.site_header = 'Proficere Admin Dashboard'
+#admin.site.register(SecurityQuestion, SecurityQuestionAdmin)
+#admin.site.register(UserProfile, UserProfileAdmin)
+#admin.site.register(AddressType, AddressTypeAdmin)
+#admin.site.register(Country, CountryAdmin)
+#admin.site.register(State, StateAdmin)
+#admin.site.register(Address, AddressAdmin)
+#admin.site.register(UserAddress, UserAddressAdmin)
+#admin.site.register(PhoneType, PhoneTypeAdmin)
+#admin.site.register(CountryExchange, CountryExchangeAdmin)
+#admin.site.register(Phone, PhoneAdmin)
+#admin.site.register(UserPhone, UserPhoneAdmin)
+#admin.site.register(EmailType, EmailTypeAdmin)
+#admin.site.register(EmailAddress, EmailAddressAdmin)
